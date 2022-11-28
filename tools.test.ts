ac8f79f57d9fe4fba16cbe90e79b7ae5272579d6
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-28 22:46:28 +0100

    finished

---
+++ tools.test.ts
@@ -0,0 +1,24 @@
+import { surround } from './tools';
+import { describe, expect, it } from 'vitest';
+
+describe('surround()', () => {
+	it("returns various brackets correctly", () => {
+		expect(surround('test', '[]')).toBe('[test]');
+		expect(surround('test', '()')).toBe('(test)');
+		expect(surround('test', '{}')).toBe('{test}');
+	});
+	it("returns similar characters correctly", () => {
+		expect(surround('test', '**')).toBe('*test*');
+		expect(surround('test', '||')).toBe('|test|');
+	});
+	it("fails gracefully on wrong number of letters", () => {
+		expect(surround('test', '***')).toBe('test');
+		expect(surround('test', '||sjkdfjskdfj')).toBe('test');
+		expect(surround('test', 'x')).toBe('test');
+	});
+	it("fails gracefully on empty values", () => {
+		expect(surround('test', '')).toBe('test');
+		expect(surround('', '')).toBe('');
+		expect(surround('', '[]')).toBe('[]');
+	});
+});
---
+++ tools.ts
@@ -0,0 +1,16 @@
+/**
+* Surrounds a string with two letters
+*
+* tools.surrounds('primary', '[]');
+*
+* '[primary]'
+*/
+export const surround = (text: string, twoLetters: string) => {
+	if (twoLetters.length != 2) {
+		return text;
+	} else {
+		const first = twoLetters[0];
+		const second = twoLetters[1];
+		return first + text + second;
+	}
+}
