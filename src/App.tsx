5b8804c337226ae78e6921d3c2170792629e4de5
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-28 23:01:06 +0100

    added surround to site

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
+import * as tools from './tools';
 
 const message = 'this is a sentence';
 
 function App() {
 	return (
 		<div className="App">
-			<p>Message: {message}</p>
+			<p>Message: {tools.surround(message, '[]')}</p>
+			<p>Message: {tools.surround(message, '()')}</p>
+			<p>Message: {tools.surround(message, '{}')}</p>
+			<p>Message: {tools.surround(message, '__')}</p>
+			<p>Message: {tools.surround(message, '><')}</p>
+			<p>Message: {tools.surround(message, 'ksjdfskdfj')}</p>
 		</div>
 	);
 }
